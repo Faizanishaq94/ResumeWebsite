2f3c30ccb0bec08bc77263ca8ddd5c00fd5c1c2e
Faizanishaq94 <faizanishaq94@gmail.com>
2018-08-27 00:06:33 -0400

    Make Incremental Project Section Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
   buttons = [true, false]; //new
+  currentSlideNum = 1;
+  maxSlideNum = 11;
+  currentSlideShowPhoto : string = '../assets/loyalty' + this.currentSlideNum + '.PNG';
   relatedCourses : string[] = ["Intro Computer Programming [Python] ","Software Design [Java]","Object Oriented Programming [Java]",
   "Program Design/Data Structures [Java]","Applied Programming [PHP]","Database Fundamentals [SQL]","Multimedia & Web Design [HTML/CSS]",
   "System Analysis and Design","IT Architecture Fundamentals","Discrete Structures"];//new
-  options : string[] = ["About Me", "Experience", "Education", "My Skills", "Contact"];
-  relations : string[] = ["/about-me", "/experience", "/education", "/my-skills", "/contact"];
-  nav = {
-    toggle : false
-  }
+
  
-  getRoute(route : string) {
-    let value : string = "/";
-    for(let i of value.split(" ")) {
-      value += i.toLowerCase() + "-";
-    }
-    return value.substring(0, value.length - 1);
-  }
+  // getRoute(route : string) {
+  //   let value : string = "/";
+  //   for(let i of value.split(" ")) {
+  //     value += i.toLowerCase() + "-";
+  //   }
+  //   return value.substring(0, value.length - 1);
+  // }
 
   openTab(num : number) {
     this.buttons = [false,false];
     this.buttons[num] = true;
+  }
+
+  changeCurrent(num : number) {
+    let photoLink = "../assets/loyalty";
+    if (num == -1) {
+      photoLink += this.currentSlideNum == 1 ? this.maxSlideNum : --this.currentSlideNum;
+    }
+    else {
+      photoLink += this.currentSlideNum == this.maxSlideNum ? '1' : ++this.currentSlideNum;
+    }
+    this.currentSlideShowPhoto = photoLink + '.PNG';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SideNavButtonComponent } from './side-nav-button/side-nav-button.component';
 import { HeaderComponent } from './header/header.component';
-import { AboutMeComponent } from './about-me/about-me.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
-import { ContactComponent } from './contact/contact.component';
-import { EducationComponent } from './education/education.component';
+
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { NonMajorExpComponent } from './experience/non-major-exp/non-major-exp.component';
@@ -20,11 +18,8 @@ import { NonMajorExpComponent } from './experience/non-major-exp/non-major-exp.c
     AppComponent,
     SideNavButtonComponent,
     HeaderComponent,
-    AboutMeComponent,
     ExperienceComponent,
     MySkillsComponent,
-    ContactComponent,
-    EducationComponent,
     FooterComponent,
     NonMajorExpComponent
   ],
