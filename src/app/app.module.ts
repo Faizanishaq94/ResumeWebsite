36d94526b83a6436cc1e283dc4b1c250cbf7c88c
Faizanishaq94 <faizanishaq94@gmail.com>
2018-08-27 17:40:38 -0400

    Make Website Up and Running 08-27-18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,12 @@ import { NgModule } from '@angular/core';
 import { MatTabsModule, MatSidenavModule, MatIconModule, MatSliderModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SideNavButtonComponent } from './side-nav-button/side-nav-button.component';
-import { HeaderComponent } from './header/header.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { MySkillsComponent } from './my-skills/my-skills.component';
-
-import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
-import { NonMajorExpComponent } from './experience/non-major-exp/non-major-exp.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SideNavButtonComponent,
-    HeaderComponent,
-    ExperienceComponent,
-    MySkillsComponent,
-    FooterComponent,
-    NonMajorExpComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
